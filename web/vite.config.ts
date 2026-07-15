8b071e2f6c219c1ff81b1cb34ceeeb4d6cdd49ed
Nekena RABEHARIVONJY <safnekena1@gmail.com>
2026-07-15 09:29:00 +0300

    remove comments

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -7,7 +7,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
